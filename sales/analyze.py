92e27380a06d3aea78717c9b7b1fe9b308dd05be
MeadowView <mv3142@gmail.com>
2020-03-14 17:10:54 -0700

    updated

--- sales/analyze.py
+++ sales/analyze.py
@@ -6,14 +6,24 @@ import csv
 import datetime
 from datetime import date
 
+class DataStore:
+	D = dict()
+	def __init__(self):
+			None
+
+	def add(self, datestr, data):
+			self.D[datestr] = data
+
 class Data:
 	W = ['월', '화', '수', '목', '금', '토', '일']
 	D = dict()
+
 	def __init__(self, datestr):
+		self.datastr = datestr
 		self.date = date.fromisoformat(datestr)
 		
-	def add(item, count):
-		D[item] = count
+	def add(self, item, count):
+		self.D[item] = count
 
 	def print(self):
 		print("Data (%s %s)"%(self.date, self.W[self.date.weekday()]))
@@ -27,7 +37,9 @@ for xlsf in glob.glob("data/*.xls"):
 		xls = pd.read_excel(xlsf)
 		xls.to_csv(csvf)
 		print("%s converted to %s"%(xlsf, csvf))
-				
+
+datastore = DataStore()
+data = None
 for csvf in glob.glob("data/*.csv"):
 	with open(csvf, 'r') as csvfile:
 		csvrd = csv.reader(csvfile, delimiter=',')
@@ -35,6 +47,7 @@ for csvf in glob.glob("data/*.csv"):
 			if (row[0] == '0'):
 				datestr = row[2].split(" ")[2]
 				data = Data(datestr)
-				data.print()
-			else:
-		                                
+				datastore.add(datestr, data)
+			elif (row[0].isnumeric() and int(row[0]) > 1):
+				print(row[2], row[4])
+				data.add(row[2], int(row[4].replace(',', '')))
