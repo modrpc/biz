34b622cc102dc304f223502ae073c31558624109
MeadowView <mv3142@gmail.com>
2020-04-21 23:12:59 +0900

    updated

--- finance/analyze.py
+++ finance/analyze.py
@@ -6,21 +6,27 @@ import csv
 import datetime
 from datetime import date
 
-class Database:
-	D = dict()
+class Account:
 	def __init__(self):
-			None
+		self._db = dict()
+		self._balance = 0
+		None
 
-	def add(self, datestr, data):
-			self.D[datestr] = data
+	def deposit(self, datetime, typ, who, amount):
+		self._balance = self._balance + amount
+
+	def withdraw(self, datatime, typ, who, amount):
+		self._balance = self._balance - amount
+
+	def balance(self):
+		return self._balance
 
 class Transaction:
-	def __init__(self, trstr):
-		self.datastr = datestr
-		self.date = date.fromisoformat(datestr)
-		self.sender = 
-		self.deposit = 
-		self.withdraw = 
+	def __init__(self, xact):
+		#self.date = date.fromisoformat(datestr)
+		self.sender = None
+		self.deposit = None
+		self.withdraw = None
 		
 	def add(self, item, count):
 		self.D[item] = count
@@ -38,10 +44,20 @@ for xlsf in glob.glob("*.xls"):
 		xls.to_csv(csvf)
 		print("%s converted to %s"%(xlsf, csvf))
 
-datastore = DataStore()
+acct = Account()
 data = None
 for csvf in glob.glob("*.csv"):
 	with open(csvf, 'r') as csvfile:
 		csvrd = csv.reader(csvfile, delimiter=',')
 		for row in csvrd:
-			print(row)
+			dt = datetime.datetime.strptime(row[2], "%Y.%m.%d %H:%M:%S")
+			typ = row[3]
+			who = row[4]
+			withdraw = int(row[5])
+			deposit = int(row[6])
+			if (withdraw != 0):
+				acct.withdraw(dt, typ, who, withdraw)
+			elif (deposit != 0):
+				acct.deposit(dt, typ, who, deposit)
+			print("%s %s with=%d dep=%d"%(typ, who, withdraw, deposit))
+			print(acct.balance())
