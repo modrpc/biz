49b58b3bfbfa2abace8263e6ccf84e5d17d1aabf
MeadowView <mv3142@gmail.com>
2020-06-18 16:10:03 +0900

    updated

--- sales/analyze.py
+++ sales/analyze.py
@@ -18,17 +18,27 @@ class Items:
 			return
 		self.dict[item] = 0
 
+	def is_valid(self, item):
+		if item in self.dict:
+			return True
+		else:
+			return False
+
 	def list(self):
-		if not sorted:
+		if not self.sorted:
 			self.sorted_list = sorted(self.dict.keys())
 			nitems = 0
-			for item in sorted_list:
+			for item in self.sorted_list:
 				self.dict[item] = nitems
 				nitems = nitems + 1
+			self.sorted = True
 		return self.sorted_list
 
 	def size(self):
 		return len(self.list())
+
+	def item_index(self, name):
+		return self.dict[name]
 
 
 items = Items()
@@ -46,41 +56,43 @@ def build_csv_paths(dir):
 			print('%s converted to %s'%(xlspath, csvpath))
 	return csvpaths
 
-def build_sorted_item_list(salesL):
-	itemD = {}
-	for sales in salesL:
-		for item in sales:
-			if item[0] in itemD:
-				continue
-			itemD[item[0]] = 0
-			print(item[0])
-
-def build_sales_data(items)
+def build_sales_list(sales):
+	ilst = [0 for _ in range(items.size())]
+	for sale in sales:
+		ilst[items.item_index(sale[0])] = int(sale[1])
+	return ilst
+	
 
 def build_sales_dataframe(csvpaths):
 	global items
 	datesL = []
-	salesL = []
 	itemsL = []
+	salesData = []
 	for csvpath in csvpaths:
 		with open(csvpath, 'r') as csvfile:
-			salesD = {}
 			nrow = 0
-
 			csvrd = csv.reader(csvfile, delimiter=',')
 			datesL.append(csvpath.replace('/06', '').replace('.csv', ''))
 
 			for row in csvrd:
 				nrow = nrow + 1
 				if nrow < 4 or row[2] == '합  계': continue
-				
-				salesD[row[2]] = int(row[4])
 				items.add(row[2])
-			salesDF = pd.DataFrame(pd.Series(salesD))
-			salesL.append(salesDF)
+
+	for csvpath in csvpaths:
+		with open(csvpath, 'r') as csvfile:
+			salesL = []
+			csvrd = csv.reader(csvfile, delimiter=',')
+			for row in csvrd:
+				if (items.is_valid(row[2])):
+					salesL.append([row[2], row[4]])
+			lst = build_sales_list(salesL)
+			print(lst)
+			salesData.append(lst)
 
 	dateIdx = pd.to_datetime(datesL)
-	return pd.DataFrame(data=salesL, index=dateIdx, columns=items.list())
+	print(salesData)
+	return pd.DataFrame(data=salesData, index=dateIdx, columns=items.list())
 
 def is_weekday(pdt):
 	return pdt.day_name() in ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
@@ -92,9 +104,9 @@ def run(dir):
 	csvpaths = build_csv_paths(dir)
 	salesDF = build_sales_dataframe(csvpaths)
 
-	print(salesDF.index)
-	print(salesDF.columns)
-	print(salesDF['단팥빵'])
+	#print(salesDF.index)
+	#print(salesDF.columns)
+	#print(salesDF['단팥빵'])
 	#print(salesDF[pd.to_datetime('2020-06-01')]['단팥빵'])
 
 if __name__ == '__main__':
